9cd2f64b25cd5003131d5038baee9bb364a2b2f6
Nikita Dedkov <dedkovnd@gmail.com>
2020-05-20 21:59:14 +0300

    add app.js

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,72 @@
+import {Team, ErrorRepository} from "../app.js";
+import sum from "../basic";
+
+// import sum from "../basic";
+
+test('don t double character ', () => {
+  class Character {
+  constructor(props) {
+    this.name = props.name
+    this.attack = props.attack
+    this.defence = props.defence
+    this.level = 1
+    this.health = props.health
+  }
+}
+class Bowman extends Character {
+  constructor(){
+    super({name:"Bowman", attack: 25, defence: 25})
+  }
+}
+class Swordsman extends Character {
+  constructor(){
+    super({name:"Swordsman", attack: 40, defence: 10})
+  }
+}
+class Magician extends Character {
+  constructor(){
+    super({name:"Magician", attack: 10, defence: 40})
+  }
+}
+class Undead extends Character {
+  constructor(){
+    super({name:"Undead", attack: 10, defence: 40})
+  }
+}
+class Zombie extends Character {
+  constructor(){
+    super({name:"Zombie", attack: 40, defence: 10})
+  }
+}
+class Daemon extends Character {
+  constructor(){
+    super({name:"Daemon", attack: 10, defence: 40})
+  }
+}
+let team = new Team();
+let bowman = new Bowman();
+let swordsman = new Swordsman();
+let daemon = new Daemon();
+let zombie = new Zombie();
+
+team.addChar(bowman)
+  team.addAllChar(bowman,swordsman,bowman,bowman,daemon)
+  team.addChar(zombie)
+  team.addChar(zombie)
+
+});
+
+/////////
+
+test('error "not to string"', () => {
+  let numTestOne = 22;
+  try {
+    numTestOne;
+    if (typeof numTestOne !== "string") {
+      throw new ErrorRepository (code = 97)
+    }
+  } catch (err) {
+    err.translate(code);
+  }
+});
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,41 @@
 // TODO: write your code here
-import sum from './basic';
+// import sum from './basic';
 
-console.log('worked');
+// console.log('worked');
+//
+// console.log(sum([1, 2]));
 
-console.log(sum([1, 2]));
+export class Team {
+  constructor() {
+    this.members = new Set();
+  }
+  addChar(char) {
+    this.members.add(char)
+  }
+  addAllChar(...chars) {
+    for (let char of chars){
+      this.members.add(char)
+    }
+  }
+  toArrayChar() {
+    return Array.from(this.members.values());
+  }
+}
+
+//////////////////
+export class ErrorRepository extends Error{
+  constuctor(code){
+    this.code = code;
+  }
+  translate(code){
+    const myErrors = new Map ([
+      [97,"not a string"],
+      [98,"not a number from to ten"],
+      [99,"number less than a hundred"]
+    ])
+    console.log(myErrors.get(code))
+    if (myErrors.get(code) === undefined) {
+      throw new Error ("Unknown error")
+    }
+  }
+}
